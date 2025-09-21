a523e99f5d79929147edf54f1d1fa10c98396135
netcrawlerr <abdulmejidamk@gmail.com>
2025-09-21 17:00:59 +0300

    "fix: font issues on mobile"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto_Condensed, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto_Condensed({
   subsets: ["latin"],
+  weight: "400",
+  variable: "--font-hero",
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
+  variable: "--font-mono",
 });
 
 export const metadata: Metadata = {
@@ -29,38 +30,18 @@ export const metadata: Metadata = {
     "F1 standings",
     "F1 Hub",
     "F1 app",
-    "F1 App",
   ],
   authors: [{ name: "Mejid Kedir", url: "https://github.com/netcrawlerr" }],
   creator: "Mejid (netcrawler)",
   themeColor: "#FF1800",
-  openGraph: {
-    type: "website",
-    url: "https://f1hub.netcrawler.dev",
-    title: "F1 Hub – Your Ultimate Formula 1 Companion",
-    description:
-      "Stay updated with the latest Formula 1 races, schedules, standings, and news. F1 Hub delivers everything a true F1 fan needs in one place.",
-    // images: [
-    //   {
-    //     url: "https://f1hub.net/og-image.png",
-    //     width: 1200,
-    //     height: 630,
-    //     alt: "F1 Hub App Preview",
-    //   },
-    // ],
-  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${roboto.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
